2cbf389d8383d0f76af0cc2edadd11832bb118e3
Nicholas Mayne <Jetski5822@hotmail.com>
2013-05-27 23:18:33 +0100

    changing to reflect the way I did Wave

--- Hubs/ContentHub.cs
+++ Hubs/ContentHub.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using NGM.OperationalTransformation.Models;
@@ -8,6 +11,7 @@ using Orchard.ContentManagement;
 using Orchard.ContentManagement.Aspects;
 using Orchard.Environment;
 using Orchard.Security;
+using Orchard.Services;
 
 namespace NGM.OperationalTransformation.Hubs {
     //https://github.com/SignalR/SignalR/wiki/Hubs
@@ -15,13 +19,16 @@ namespace NGM.OperationalTransformation.Hubs {
     public class ContentHub : Hub {
         private readonly Work<IContentManager> _workContentManager;
         private readonly Work<IAuthenticationService> _workAuthenticationService;
+        private readonly IClock _clock;
 
         private const string ContentGroupNameFormat = "ContentItem-{0}";
 
         public ContentHub(Work<IContentManager> workContentManager,
-            Work<IAuthenticationService> workAuthenticationService) {
+            Work<IAuthenticationService> workAuthenticationService,
+            IClock clock) {
             _workContentManager = workContentManager;
             _workAuthenticationService = workAuthenticationService;
+            _clock = clock;
         }
 
         private IContentManager ContentManager {
@@ -32,30 +39,26 @@ namespace NGM.OperationalTransformation.Hubs {
             get { return _workAuthenticationService.Value; }
         }
 
+        public override Task OnConnected() {
+            return Clients.All.joined(Context.ConnectionId, _clock.UtcNow.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public override Task OnDisconnected() {
+            return Clients.All.leave(Context.ConnectionId, _clock.UtcNow.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public override Task OnReconnected() {
+            return Clients.All.rejoined(Context.ConnectionId, _clock.UtcNow.ToString(CultureInfo.InvariantCulture));
+        }
+
         public void Join(int contentItemId) {
             Join(contentItemId, false);
         }
 
         public void Join(int contentItemId, bool reconnecting) {
-            var user = AuthenticationService.GetAuthenticatedUser();
-            var content = ContentManager.Get(contentItemId);
+            IUser user = AuthenticationService.GetAuthenticatedUser() ?? new UnauthenticatedUser(Context.ConnectionId);
 
-            Clients.Caller.id = user.Id;
-            Clients.Caller.username = user.UserName;
-            Clients.Caller.activeContent = contentItemId;
-
-            var isUserOwner = content.As<ICommonPart>().Owner == user;
-            var userViewModel = new UserViewModel(user);
-            var contentGroupName = ContentGroupName(contentItemId);
-
-            if (reconnecting) {
-            }
-            else {
-            }
-            // Tell the people who are editing this content that you are viewing it and cmaybe editing it.
-            Clients.Group(contentGroupName).addUser(userViewModel, contentGroupName, isUserOwner).Wait();
-            // Add the caller to the group so they receive content updates
-            Groups.Add(Context.ConnectionId, contentGroupName);
+            OnUserInitialize(contentItemId, user, reconnecting);
         }
 
         public void SendPatches(int contentItemId, string elementId, string elementName, int contentItemVersion, List<PatchModel> patches) {
@@ -72,6 +75,25 @@ namespace NGM.OperationalTransformation.Hubs {
             Clients.Caller.changeAcknowledged(draftContentItem.Version);
 
             Clients.OthersInGroup(ContentGroupName(contentItemId)).applyPatches(elementId, contentItemVersion, patches);
+        }
+
+        private void OnUserInitialize(int contentItemId, IUser user, bool reconnecting) {
+            if (!reconnecting) {
+                Clients.Caller.id = user.Id;
+                Clients.Caller.username = user.UserName;
+                Clients.Caller.activeContent = contentItemId;
+            }
+
+            var content = ContentManager.Get(contentItemId);
+
+            var isOwner = content.As<ICommonPart>().Owner == user;
+            var userViewModel = new UserViewModel(user);
+            var contentGroupName = ContentGroupName(contentItemId);
+
+            // Tell the people who are editing this content that you are viewing it and cmaybe editing it.
+            Clients.Group(contentGroupName).addUser(userViewModel, contentGroupName, isOwner).Wait();
+            // Add the caller to the group so they receive content updates
+            Groups.Add(Context.ConnectionId, contentGroupName);
         }
 
         private static string ContentGroupName(int contentItemId) {
@@ -106,4 +128,18 @@ namespace NGM.OperationalTransformation.Hubs {
 
         public string UserName { get; private set; }
     }
+
+    public class UnauthenticatedUser : IUser {
+        public UnauthenticatedUser(string conntentionId) {
+            ConntentionId = conntentionId;
+        }
+
+        public string ConntentionId { get; private set; }
+
+        public ContentItem ContentItem { get; private set; }
+        public int Id { get; private set; }
+        public string UserName { get; private set; }
+        public string Email { get; private set; }
+    }
+
 }
